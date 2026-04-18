bbf88f01dcb0b97201945a0f3d844044148e1a3f
Altan <altnunl29@gmail.com>
2026-04-18 18:11:03 +0300

    favicon added

--- app/views/layout.js
+++ app/views/layout.js
@@ -17,6 +17,7 @@ function renderLayout({
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <link rel="icon" href="/favicon.ico">
   <!-- 🔥 MONETAG POPUNDER SCRIPT (DOĞRU) -->
   <script>
   (function(s){
